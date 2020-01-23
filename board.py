ffa2005a14650f166662db2737165c6f6f1be21e
David <43117846+davidria@users.noreply.github.com>
2020-01-23 14:06:36 -0500

    Update board.py

--- board.py
+++ board.py
@@ -1,6 +1,8 @@
-cat = 1
-mouse = -1
-cheese = 0
+from os import system
+
+cat = -7
+mouse = 1
+cheese = 6
 
 board = [[' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
         [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
@@ -23,14 +25,22 @@ board = [[' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
 
 #def possibleMovesMouse(board):
 
-#def clearScreen():
+def clearScreen():
+    os = platform.system().lower()
+    
+    if 'windows' = os:
+        os.sysem('cls')
+    else :
+        os.system('clear')
 
-def endGame(board):
-    return checkWinner(board, cat) or checkWinner(board, mouse)
+#def checkScore(board):
     
 def checkWinner(board, player):
+    if (player == 0 or player == 7)
+        return True
+        
+    return False
     
-
 def printBoard(board,mouse,cat,cheese):
     
 #def mouseTurn(mouse,cat):
@@ -42,5 +52,19 @@ def printBoard(board,mouse,cat,cheese):
 #def move(x,y,player):
 
 
-#def main():
+def main():
+
+    if checkWinner(board, cat):
+        clear_screen()
+        print_board(board,mouse,cat,cheese)
+        print ("CAT WINS")
+        
+    elif check_winner(board, mouse):
+        clear_screen()
+        print_board(board,mouse,cat,cheese)
+        print ("MOUSE WINS")
+    else:
+        clear_screen()
+        print_board(board,mouse,cat,cheese)
+        print ("DRAW")
 
